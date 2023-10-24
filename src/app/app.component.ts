ccbe8e0450d96963afcb79229996166c7a94a017
Abdulhamid254 <nassurabdulhamid49@gmail.com>
2023-10-24 05:58:06 -0700

    Adding/deleting/filtering servers & printing the report

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,8 @@ import { CustomResponse } from './interface/custom-response';
 import { ServerService } from './service/server.service';
 import { DataState } from './enum/data.state.enum';
 import { Status } from './enum/status.enum';
+import { NgForm } from '@angular/forms';
+import { Server } from './interface/server';
 
 @Component({
   selector: 'app-root',
@@ -16,14 +18,19 @@ export class AppComponent implements OnInit{
   readonly DataState = DataState;
   readonly Status = Status;
   private filterSubject = new BehaviorSubject<string>('');
+  private dataSubject = new BehaviorSubject<CustomResponse>(null); // copy of our response
   filterStatus$ = this.filterSubject.asObservable();
+  private isLoading = new BehaviorSubject<boolean>(false);
+  isLoading$ = this.isLoading.asObservable();
 
   constructor( private serverService: ServerService){}
 
   ngOnInit(): void {
     this.appState$ = this.serverService.servers$.pipe(
       map(response => {
-        return { dataState: DataState.LOADED_STATE, appData: response}
+        // saving a copy of the response to the datasubject so as to use it somewhere else
+        this.dataSubject.next(response);
+        return { dataState: DataState.LOADED_STATE, appData: {...response, data: { servers: response.data.servers.reverse()}}}
       }),
       startWith({ dataState: DataState.LOADING_STATE}),
       catchError((error: string) => {
@@ -32,6 +39,99 @@ export class AppComponent implements OnInit{
         })
       })
     )
+  }
+
+  pingServer(ipAddress: string): void {
+    this.filterSubject.next(ipAddress); // loading spinner
+    this.appState$ = this.serverService.ping$(ipAddress)
+      .pipe(
+        map(response => {
+          // looping to ensure that the server the user is ping matches with the one in the database
+          const index = this.dataSubject.value.data.servers.findIndex(server =>  server.id === response.data.server.id);
+          this.dataSubject.value.data.servers[index] = response.data.server;
+          this.notifier.onDefault(response.message);
+          this.filterSubject.next('');
+          return { dataState: DataState.LOADED_STATE, appData: this.dataSubject.value }
+        }),
+        startWith({ dataState: DataState.LOADED_STATE, appData: this.dataSubject.value }),
+        catchError((error: string) => {
+          this.filterSubject.next('');
+          this.notifier.onError(error);
+          return of({ dataState: DataState.ERROR_STATE, error });
+        })
+      );
+  }
+
+  saveServer(serverForm: NgForm): void {
+    this.isLoading.next(true);
+    this.appState$ = this.serverService.save$(serverForm.value as Server)
+      .pipe(
+        map(response => {
+          this.dataSubject.next(
+            {...response, data: { servers: [response.data.server, ...this.dataSubject.value.data.servers] } }
+          );
+          this.notifier.onDefault(response.message);
+          document.getElementById('closeModal').click(); // closing the modal the js way
+          this.isLoading.next(false);
+          serverForm.resetForm({ status: this.Status.SERVER_UP }); // by default we want the server status to be up
+          return { dataState: DataState.LOADED_STATE, appData: this.dataSubject.value }
+        }),
+        startWith({ dataState: DataState.LOADED_STATE, appData: this.dataSubject.value }),
+        catchError((error: string) => {
+          this.isLoading.next(false);
+          this.notifier.onError(error);
+          return of({ dataState: DataState.ERROR_STATE, error });
+        })
+      );
+}
+
+  filterServers(status: Status): void {
+    this.appState$ = this.serverService.filter$(status, this.dataSubject.value)
+      .pipe(
+        map(response => {
+          this.notifier.onDefault(response.message);
+          return { dataState: DataState.LOADED_STATE, appData: response };
+        }),
+        startWith({ dataState: DataState.LOADED_STATE, appData: this.dataSubject.value }),
+        catchError((error: string) => {
+          this.notifier.onError(error);
+          return of({ dataState: DataState.ERROR_STATE, error });
+        })
+      );
+  }
+
+  deleteServer(server: Server): void {
+    this.appState$ = this.serverService.delete$(server.id)
+      .pipe(
+        map(response => {
+          this.dataSubject.next(
+            { ...response, data:
+              { servers: this.dataSubject.value.data.servers.filter(s => s.id !== server.id)} }
+          );
+          this.notifier.onDefault(response.message);
+          return { dataState: DataState.LOADED_STATE, appData: this.dataSubject.value }
+        }),
+        startWith({ dataState: DataState.LOADED_STATE, appData: this.dataSubject.value }),
+        catchError((error: string) => {
+          this.notifier.onError(error);
+          return of({ dataState: DataState.ERROR_STATE, error });
+        })
+      );
+  }
+
+  printReport(): void {
+    this.notifier.onDefault('Report downloaded');
+    // window.print(); // this is when you want to print it as a pdf
+    // the below one we want it as a exxcel
+    let dataType = 'application/vnd.ms-excel.sheet.macroEnabled.12';
+    let tableSelect = document.getElementById('servers');
+    let tableHtml = tableSelect.outerHTML.replace(/ /g, '%20');
+    let downloadLink = document.createElement('a');
+    document.body.appendChild(downloadLink);
+    downloadLink.href = 'data:' + dataType + ', ' + tableHtml;
+    downloadLink.download = 'server-report.xls';
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
   }
 
 
